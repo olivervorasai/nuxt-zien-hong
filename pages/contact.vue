9f01a17c508554c162fcd81586ca36a8c2d7dcc9
olivervorasai <olivervorasai@gmail.com>
2020-10-19 04:16:51 -0700

    Contact: Added netlify standard form for testing

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,31 @@
 <template>
   <v-container>
+    <v-row>
+      <form name="netlify-test-form" method="POST" data-netlify="true">
+        <p>
+          <label>Your Name: <input type="text" name="test-name" /></label>
+        </p>
+        <p>
+          <label>Your Email: <input type="email" name="test-email" /></label>
+        </p>
+        <p>
+          <label
+            >Your Role:
+            <select name="test-role[]" multiple>
+              <option value="leader">Leader</option>
+              <option value="follower">Follower</option>
+            </select></label
+          >
+        </p>
+        <p>
+          <label>Message: <textarea name="test-message"></textarea></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </v-row>
+
     <v-row justify="center">
       <v-col cols="12" sm="10" md="8" lg="6" xl="4">
         <v-form name="contact" method="POST" data-netlify="true">
