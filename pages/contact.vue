6f4a5d876c67b65e431458f7c3e57f88f8f3bff7
olivervorasai <olivervorasai@gmail.com>
2020-10-19 03:54:46 -0700

    Contact: Added contact us page

---
+++ pages/contact.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" sm="10" md="8" lg="6" xl="4">
+        <v-form name="contact" method="POST" data-netlify="true">
+          <v-container>
+            <v-row>
+              <v-text-field
+                label="Full Name"
+                required
+                solo-inverted
+              ></v-text-field>
+            </v-row>
+            <v-row>
+              <v-text-field
+                label="E-mail"
+                required
+                solo-inverted
+              ></v-text-field>
+            </v-row>
+            <v-row>
+              <v-textarea label="Message" required solo-inverted></v-textarea>
+            </v-row>
+            <v-row>
+              <v-btn type="submit"> submit </v-btn>
+            </v-row>
+          </v-container>
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
